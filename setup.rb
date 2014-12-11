60887812df6a5beeaa56a0e65dfd3e7094953739
Tyler B <tylerb@win2k3.personal.test.com>
2014-12-10 16:22:16 -0800

    Fixing up script

--- setup.rb
+++ setup.rb
@@ -6,15 +6,23 @@ require 'pathname'
 include FileUtils
 
 def install_command_line_tools()
-  repository_url = 'https://github.cerner.com/utils/osx-dev-environment-automation.git'
-  `git clone #{repository_url}`
-  `ruby ./osx-dev-environment-automation/xcode-commandline-tools-install.rb`
+  repository_url = 'http://github.cerner.com/utils/osx-dev-environment-automation.git'
+  puts "Cloning #{repository_url}"
+  system("git clone #{repository_url}")
+
+  puts 'Installing command line tools'
+  system("ruby ./osx-dev-environment-automation/xcode-commandline-tools-install.rb")
+  
+  puts 'Removing command line tools automation script'
   FileUtils.rm_rf(File.join(Dir.getwd, 'osx-dev-environment-automation'))
 end
 
 def execute_sprout_setup
-  `gem install bundler`
-  `bundle exec soloist`
+  puts 'Installing bundler'
+  system("gem install bundler")
+
+  puts 'Running sprout setup'
+  system("bundle exec soloist")
 end
 
 install_command_line_tools()
